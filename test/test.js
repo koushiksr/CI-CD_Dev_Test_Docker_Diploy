919f5f8aff61849ae521e6b47d3aaebeb4eeec5f
k <k@kali>
2024-08-03 21:19:36 +0530

    Add CI/CD workflow and tests

---
+++ test/test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => res.status(200).send('Hello World!'));
+
+describe('GET /', () => {
+  it('respond with Hello World', (done) => {
+    request(app)
+      .get('/')
+      .expect('Hello World!', done);
+  });
+});
